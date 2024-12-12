9530ed90e398011124cddbf849a1d4f09a827b1b
张楷鸿 <aaronkhzhang@kugou.net>
2024-12-12 11:05:43 +0800

    Initial Commit

---
+++ CaveWave/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CaveWave
+//
+//  Created by 张楷鸿 on 2024/12/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CaveWave/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CaveWave
+//
+//  Created by 张楷鸿 on 2024/12/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
